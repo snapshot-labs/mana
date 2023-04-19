d9e5135559d9175ea9590d69f62856e60c7e9ebc
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-04-19 15:52:39 +0200

    chore: make cache never expire

--- src/eth/execution.ts
+++ src/eth/execution.ts
@@ -2,32 +2,17 @@ import { clients } from '@snapshot-labs/sx';
 import { wallet } from './dependencies';
 
 type Cache = {
-  [key: string]: {
-    timestamp: number;
-    receipt: any;
-  };
+  [key: string]: any;
 };
 
-const CACHE_TIMEOUT = 5 * 60 * 1000;
 const CACHED_RECEIPTS: Cache = {};
 
 const client = new clients.EvmEthereumTx();
 
-function clearExpiredCache() {
-  const now = Date.now();
-  Object.keys(CACHED_RECEIPTS).forEach(key => {
-    if (now - CACHED_RECEIPTS[key].timestamp > CACHE_TIMEOUT) {
-      delete CACHED_RECEIPTS[key];
-    }
-  });
-}
-
 export async function execute(space: string, proposalId: number, executionParams: string) {
-  clearExpiredCache();
-
   const cacheKey = `execute-${space}-${proposalId}-${executionParams}`;
   if (CACHED_RECEIPTS[cacheKey]) {
-    return CACHED_RECEIPTS[cacheKey].receipt;
+    return CACHED_RECEIPTS[cacheKey];
   }
 
   const receipt = await client.execute({
@@ -37,20 +22,15 @@ export async function execute(space: string, proposalId: number, executionParams
     executionParams
   });
 
-  CACHED_RECEIPTS[cacheKey] = {
-    timestamp: Date.now(),
-    receipt
-  };
+  CACHED_RECEIPTS[cacheKey] = receipt;
 
   return receipt;
 }
 
 export async function executeQueuedProposal(executionStrategy: string, executionParams: string) {
-  clearExpiredCache();
-
   const cacheKey = `executeQueuedProposal-${executionStrategy}-${executionParams}`;
   if (CACHED_RECEIPTS[cacheKey]) {
-    return CACHED_RECEIPTS[cacheKey].receipt;
+    return CACHED_RECEIPTS[cacheKey];
   }
 
   const receipt = await client.executeQueuedProposal({
@@ -59,10 +39,7 @@ export async function executeQueuedProposal(executionStrategy: string, execution
     executionParams
   });
 
-  CACHED_RECEIPTS[cacheKey] = {
-    timestamp: Date.now(),
-    receipt
-  };
+  CACHED_RECEIPTS[cacheKey] = receipt;
 
   return receipt;
 }
