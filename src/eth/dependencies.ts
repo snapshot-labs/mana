9e548cdf44489d99f5cec6a5f4d28565360c212e
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-05-22 17:14:07 +0200

    feat: detect sponsor wallet from space address

--- src/eth/dependencies.ts
+++ src/eth/dependencies.ts
@@ -2,9 +2,31 @@ import { JsonRpcProvider } from '@ethersproject/providers';
 import { Wallet } from '@ethersproject/wallet';
 import { NonceManager } from '@ethersproject/experimental';
 
-const ethPrivkey = process.env.ETH_PRIVKEY || '';
+const ethMnemonic = process.env.ETH_MNEMONIC || '';
 const ethRpcUrl = process.env.ETH_RPC_URL || '';
 
+const paths = {
+  // SekhmetDAO
+  '0x65e4329e8c0fba31883b98e2cf3e81d3cdcac780': "m/44'/60'/0'/0/1"
+};
+
 export const provider = new JsonRpcProvider(ethRpcUrl);
-export const wallet = new Wallet(ethPrivkey, provider);
-export const signer = new NonceManager(wallet);
+
+export const createWalletProxy = (mnemonic: string) => {
+  const signers = new Map<string, NonceManager>();
+
+  return (spaceAddress: string) => {
+    const normalizedSpaceAddress = spaceAddress.toLowerCase();
+
+    if (!signers.has(normalizedSpaceAddress)) {
+      const path = paths[normalizedSpaceAddress] || "m/44'/60'/0'/0/0";
+
+      const wallet = Wallet.fromMnemonic(mnemonic, path);
+      signers.set(normalizedSpaceAddress, new NonceManager(wallet.connect(provider)));
+    }
+
+    return signers.get(normalizedSpaceAddress) as NonceManager;
+  };
+};
+
+export const getWallet = createWalletProxy(ethMnemonic);
